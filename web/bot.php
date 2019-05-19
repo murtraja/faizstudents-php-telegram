df81b7838b2ca22762c3c34c7c1c80f85889d57a
Murtaza Raja <murtraja@gmail.com>
2019-05-20 05:03:08 +0530

    adding logs

--- web/bot.php
+++ web/bot.php
@@ -3,7 +3,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "printing params";
     error_log(print_r($_POST, true));
     $update = json_decode( file_get_contents( 'php://input' ) );
-    error_log($update);
+    error_log(print_r($update, true));
 } else {
     echo "You are not a bot, are you?";
     error_log("This should be logged");
