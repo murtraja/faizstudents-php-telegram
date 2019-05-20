5992bcd3e2ab2d59e81f6f0c752145721e00da3c
Murtaza Raja <murtraja@gmail.com>
2019-05-20 05:53:30 +0530

    sending back response

--- web/bot.php
+++ web/bot.php
@@ -1,11 +1,28 @@
 <?php
+
+$API_TOKEN = getevn('API_TOKEN');
+$API_URL = 'https://api.telegram.org/bot$API_TOKEN/';
+
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "printing params";
-    error_log(print_r($_POST, true));
-    $update = json_decode( file_get_contents( 'php://input' ) );
-    error_log(print_r($update, true));
+    $message = (array) json_decode( file_get_contents( 'php://input' ) );
+    error_log("printing params");
+    error_log(print_r($message, true));
+    sendTextMessage($message, 'hello there');
 } else {
     echo "You are not a bot, are you?";
     error_log("This should be logged");
+}
+
+function sendTextMessage($message, $text) {
+    $chat_id = $message['chat']['id'];
+    $params = array(
+        "chat_id" => $chat_id,
+        "text" => $text,
+    );
+    $sendMessageUrl = $API_URL.'sendMessage/?'.http_build_query($params);
+    error_log('sendMessageUrl:'.$sendMessageUrl);
+    $response = file_get_contents($sendMessageUrl);
+    error_log("print response");
+    error_log(print_r($response, true));
 }
 ?>
