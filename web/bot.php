26cd114fb8ebbd65ab23fd19c49299306a4e142a
Murtaza Raja <murtraja@gmail.com>
2019-05-20 06:32:55 +0530

    asking for ph no

--- web/bot.php
+++ web/bot.php
@@ -2,25 +2,36 @@
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $input = json_decode( file_get_contents( 'php://input' ) );
-    error_log("printing received input");
-    error_log(print_r($input, true));
+    printVariable("Input:", $input);
+
     $message = $input->message;
-    sendTextMessage($message, 'hello there');
+    
+    $chatId = getChatId($message);
+    printVariable("Chat Id:", $chatId);
+    
+    $mobile = getMobile($chatId);
+    if($mobile === false) {
+        sendPhoneNumberRequest($chatId);
+    }
+    sendTextMessage($chatId, 'hello there');
 } else {
     echo "You are not a bot, are you?";
     error_log("This should be logged");
 }
 
-function sendTextMessage($message, $text) {
-    $chat_id = $message->chat->id;
+function sendTextMessage($chatId, $text) {
     $params = array(
-        "chat_id" => $chat_id,
+        "chat_id" => $chatId,
         "text" => $text,
     );
-    sendMessage($message, $params);
+    sendMessage($params);
 }
 
-function sendMessage($message, $params) {
+function getChatId($message) {
+    return $message->chat->id;
+}
+
+function sendMessage($params) {
     $API_TOKEN = getenv('API_TOKEN');
     $API_URL = 'https://api.telegram.org/bot'.$API_TOKEN.'/';
     $sendMessageUrl = $API_URL.'sendMessage?'.http_build_query($params);
@@ -28,5 +39,23 @@ function sendMessage($message, $params) {
     $response = file_get_contents($sendMessageUrl);
     error_log("print response");
     error_log(print_r($response, true));
+}
+
+function getMobile($chatId) {
+    $redis = new Predis\Client(getenv('REDIS_URL'));
+    if($redis->exists($chatId)) {
+        $mobile = $redis->get($chatId);
+        return $mobile;
+    }
+    return false;
+}
+
+function printVariable($text, $variable) {
+    error_log($text);
+    error_log(print_r($variable, true));
+}
+
+function sendPhoneNumberRequest($chatId) {
+    sendTextMessage($chatId, "Send phone number");
 }
 ?>
